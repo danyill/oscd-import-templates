d1b4603447bc47dd1e67e3e7e8a2c40acc0eedc4
Daniel Mulholland <dan.mulholland@gmail.com>
2023-09-19 06:14:05 +1200

    Replace functionality with scl-lib equivalents

--- oscd-import-templates.ts
+++ oscd-import-templates.ts
@@ -3,19 +3,21 @@ import { property, query, state } from 'lit/decorators.js';
 
 import { msg } from '@lit/localize';
 
+import { insertIed } from '@openenergytools/scl-lib';
 import { newEditEvent } from '@openscd/open-scd-core';
 import type { Dialog } from '@material/mwc-dialog';
-import type { Edit } from '@openscd/open-scd-core';
+import type { Checkbox } from '@material/mwc-checkbox';
+
 import type { OscdTextfield } from './foundation/components/oscd-textfield.js';
 
 import '@material/dialog';
 import '@material/mwc-button';
+import '@material/mwc-checkbox';
+import '@material/mwc-formfield';
 
 import './foundation/components/oscd-textfield.js';
 
 import { isPublic } from './foundation.js';
-import { createElement } from './foundation/elements/create.js';
-import { selector } from './foundation/identities/selector.js';
 
 function uniqueTemplateIedName(doc: XMLDocument, ied: Element): string {
   const [manufacturer, type] = ['manufacturer', 'type'].map(attr =>
@@ -103,339 +105,6 @@ function updateNamespaces(destElement: Element, sourceElement: Element) {
     });
 }
 
-function getSubNetwork(elements: Element[], element: Element): Element {
-  const existElement = elements.find(
-    item => item.getAttribute('name') === element.getAttribute('name')
-  );
-  return existElement || <Element>element.cloneNode(false);
-}
-
-/**
- * Creates and array of actions to transfer an SCL Communication section from an SCL document containing
- * ied to doc, creating an SCL Communication element if required.
- * Adds elements to existing Subnetworks where possible and creates the Subnetworks when necessary. Then
- * transfers ConnectedAP elements.
- * @param ied - SCL IED containing ConnectedAPs
- * @param doc - SCL document to receive Communication elements
- * @returns Array of actions to transfer the SCL section
- */
-function addCommunicationElements(ied: Element, doc: XMLDocument): Edit[] {
-  const edits = [];
-
-  const oldCommunicationElement = doc.querySelector(':root > Communication');
-
-  const communication =
-    oldCommunicationElement || createElement(doc, 'Communication', {});
-
-  if (!oldCommunicationElement)
-    edits.push({
-      parent: doc.querySelector(':root')!,
-      node: communication,
-      reference: null,
-    });
-
-  const connectedAPs = Array.from(
-    ied.ownerDocument.querySelectorAll(
-      `:root > Communication > SubNetwork > ConnectedAP[iedName="${ied.getAttribute(
-        'name'
-      )}"]`
-    )
-  );
-
-  const createdSubNetworks: Element[] = [];
-
-  connectedAPs.forEach(connectedAP => {
-    const newSubNetwork = <Element>connectedAP.parentElement!;
-    const oldSubNetworkMatch = communication.querySelector(
-      `:root > Communication > SubNetwork[name="${newSubNetwork.getAttribute(
-        'name'
-      )}"]`
-    );
-
-    const subNetwork =
-      oldSubNetworkMatch || getSubNetwork(createdSubNetworks, newSubNetwork);
-    const element = <Element>connectedAP.cloneNode(true);
-
-    if (!oldSubNetworkMatch && !createdSubNetworks.includes(subNetwork)) {
-      edits.push({
-        parent: communication,
-        node: subNetwork,
-        reference: null,
-      });
-
-      createdSubNetworks.push(subNetwork);
-    }
-
-    edits.push({
-      parent: subNetwork,
-      node: element,
-      reference: null,
-    });
-  });
-
-  return edits;
-}
-
-/**
- * Recursively checks from children to parents that an SCL Datatype Template called type
- * is used within an ied. When the first one is found returns true.
- * @param type - An SCL DataTypeTemplate.
- * @param ied - An SCL IED element.
- * @returns true if there is a connection, otherwise false.
- */
-function hasConnectionToIed(type: Element, ied: Element): boolean {
-  const data: Element = type.parentElement!;
-  const id = type.getAttribute('id');
-
-  if (!data || !id) return false;
-
-  if (type.tagName === 'EnumType')
-    return Array.from(
-      data.querySelectorAll(
-        `DOType > DA[type="${id}"],DAType > BDA[type="${id}"]`
-      )
-    ).some(typeChild => hasConnectionToIed(typeChild.parentElement!, ied));
-
-  if (type.tagName === 'DAType')
-    return Array.from(
-      data.querySelectorAll(
-        `DOType > DA[type="${id}"],DAType > BDA[type="${id}"]`
-      )
-    ).some(typeChild => hasConnectionToIed(typeChild.parentElement!, ied));
-
-  if (type.tagName === 'DOType')
-    return Array.from(
-      data.querySelectorAll(
-        `LNodeType > DO[type="${id}"], DOType > SDO[type="${id}"]`
-      )
-    ).some(typeChild => hasConnectionToIed(typeChild.parentElement!, ied));
-
-  return Array.from(ied.getElementsByTagName('LN0'))
-    .concat(Array.from(ied.getElementsByTagName('LN')))
-    .some(anyln => anyln.getAttribute('lnType') === id);
-}
-
-/**
- * Adds new EnumType used by ied to parent. Parent would typically be the DataTypeTemplate SCL section.
- * NOTE: Updates the EnumType id within the ied if it already exists in the parent.
- * @param ied - SCL IED
- * @param enumType - SCL EnumType (typically associated with ied)
- * @param parent - element where changes are required (e.g. DataTypeTemplates)
- * @returns a SimpleAction representing the change.
- */
-function addEnumType(
-  ied: Element,
-  enumType: Element,
-  parent: Element
-): Edit | undefined {
-  if (!hasConnectionToIed(enumType, ied)) return;
-
-  const existEnumType = parent.querySelector(
-    `EnumType[id="${enumType.getAttribute('id')}"]`
-  );
-  if (existEnumType && enumType.isEqualNode(existEnumType)) return;
-
-  if (existEnumType) {
-    // There is an `id` conflict in the project that must be resolved by
-    // concatenating the IED name with the id
-    const data: Element = enumType.parentElement!;
-    const idOld = enumType.getAttribute('id');
-    const idNew = ied.getAttribute('name')! + idOld;
-    enumType.setAttribute('id', idNew);
-
-    data
-      .querySelectorAll(
-        `DOType > DA[type="${idOld}"],DAType > BDA[type="${idOld}"]`
-      )
-      .forEach(type => type.setAttribute('type', idNew));
-  }
-
-  // eslint-disable-next-line consistent-return
-  return {
-    parent,
-    node: enumType,
-    reference: parent.querySelector('EnumType'),
-  };
-}
-
-/**
- * Adds new DAType used by ied to parent. Parent would typically be the DataTypeTemplate SCL section.
- * NOTE: Updates the DAType id within the ied if it already exists in the parent.
- * @param ied - SCL IED
- * @param dAType - SCL DAType (typically associated with ied)
- * @param parent - element where changes are required (e.g. DataTypeTemplates)
- * @returns a SimpleAction representing the change.
- */
-function addDAType(
-  ied: Element,
-  daType: Element,
-  parent: Element
-): Edit | undefined {
-  if (!hasConnectionToIed(daType, ied)) return;
-
-  const existDAType = parent.querySelector(
-    `DAType[id="${daType.getAttribute('id')}"]`
-  );
-  if (existDAType && daType.isEqualNode(existDAType)) return;
-
-  if (existDAType) {
-    // There is an `id` conflict in the project that must be resolved by
-    // concatenating the IED name with the id
-    const data: Element | null = daType.parentElement!;
-    const idOld = daType.getAttribute('id');
-    const idNew = ied.getAttribute('name')! + idOld;
-    daType.setAttribute('id', idNew);
-
-    data
-      .querySelectorAll(
-        `DOType > DA[type="${idOld}"],DAType > BDA[type="${idOld}"]`
-      )
-      .forEach(type => type.setAttribute('type', idNew));
-  }
-
-  // eslint-disable-next-line consistent-return
-  return {
-    parent,
-    node: daType,
-    reference: parent.querySelector('DAType'),
-  };
-}
-
-/**
- * Adds new DOType used by ied to parent. Parent would typically be the DataTypeTemplate SCL section.
- * NOTE: Updates the DOType id within the ied if it already exists in the parent.
- * @param ied - SCL IED
- * @param doType - SCL DOType (typically associated with ied)
- * @param parent - element where changes are required (e.g. DataTypeTemplates)
- * @returns a SimpleAction representing the change.
- */
-function addDOType(
-  ied: Element,
-  doType: Element,
-  parent: Element
-): Edit | undefined {
-  if (!hasConnectionToIed(doType, ied)) return;
-
-  const existDOType = parent.querySelector(
-    `DOType[id="${doType.getAttribute('id')}"]`
-  );
-  if (existDOType && doType.isEqualNode(existDOType)) return;
-
-  if (existDOType) {
-    // There is an `id` conflict in the project that must be resolved by
-    // concatenating the IED name with the id
-    const data: Element = doType.parentElement!;
-    const idOld = doType.getAttribute('id');
-    const idNew = ied.getAttribute('name')! + idOld;
-    doType.setAttribute('id', idNew);
-
-    data
-      .querySelectorAll(
-        `LNodeType > DO[type="${idOld}"], DOType > SDO[type="${idOld}"]`
-      )
-      .forEach(type => type.setAttribute('type', idNew));
-  }
-
-  // eslint-disable-next-line consistent-return
-  return {
-    parent,
-    node: doType,
-    reference: parent.querySelector('DOType'),
-  };
-}
-
-/**
- * Adds new LNodeType used by ied to parent. Parent would typically be the DataTypeTemplate SCL section.
- * NOTE: Updates the lNodeType id within the ied if it already exists in the parent.
- * @param ied - SCL IED
- * @param lNodeType - SCL LNodeType (typically associated with ied)
- * @param parent - element where changes are required (e.g. DataTypeTemplates)
- * @returns a SimpleAction representing the change.
- */
-function addLNodeType(
-  ied: Element,
-  lNodeType: Element,
-  parent: Element
-): Edit | undefined {
-  if (!hasConnectionToIed(lNodeType, ied)) return;
-
-  const existLNodeType = parent.querySelector(
-    `LNodeType[id="${lNodeType.getAttribute('id')}"]`
-  );
-  if (existLNodeType && lNodeType.isEqualNode(existLNodeType)) return;
-
-  if (existLNodeType) {
-    // There is an `id` conflict in the project that must be resolved by
-    // concatenating the IED name with the id
-    const idOld = lNodeType.getAttribute('id')!;
-    const idNew = ied.getAttribute('name')!.concat(idOld);
-    lNodeType.setAttribute('id', idNew);
-
-    Array.from(
-      ied.querySelectorAll(`LN0[lnType="${idOld}"],LN[lnType="${idOld}"]`)
-    )
-      .filter(isPublic)
-      .forEach(ln => ln.setAttribute('lnType', idNew));
-  }
-
-  // eslint-disable-next-line consistent-return
-  return {
-    parent,
-    node: lNodeType,
-    reference: parent.querySelector('LNodeType'),
-  };
-}
-
-/**
- * Get datatype templates used by ied to doc and return actions array for changes required.
- * Will create an SCL or DataTypeTemplates section if not present.
- * NOTE: Will adjust the ied type names if there is a conflict in type ids with the existing doc.
- * @param ied - SCL IED beingwhich uses datatype templates
- * @param doc - project where SCL datatype templates
- * @returns an array of actions consisting of LNodeType, DOType, DAType and EnumTypes
- */
-function addDataTypeTemplates(ied: Element, doc: XMLDocument): Edit[] {
-  const edits: Edit[] | undefined = [];
-
-  const dataTypeTemplates = doc.querySelector(':root > DataTypeTemplates')
-    ? doc.querySelector(':root > DataTypeTemplates')!
-    : createElement(doc, 'DataTypeTemplates', {});
-
-  if (!dataTypeTemplates.parentElement) {
-    edits.push({
-      parent: doc.querySelector('SCL')!,
-      node: dataTypeTemplates,
-      reference: null,
-    });
-  }
-
-  ied.ownerDocument
-    .querySelectorAll(':root > DataTypeTemplates > LNodeType')
-    .forEach(lNodeType =>
-      edits.push(<Edit>addLNodeType(ied, lNodeType, dataTypeTemplates!))
-    );
-
-  ied.ownerDocument
-    .querySelectorAll(':root > DataTypeTemplates > DOType')
-    .forEach(doType =>
-      edits.push(<Edit>addDOType(ied, doType, dataTypeTemplates!))
-    );
-
-  ied.ownerDocument
-    .querySelectorAll(':root > DataTypeTemplates > DAType')
-    .forEach(daType =>
-      edits.push(<Edit>addDAType(ied, daType, dataTypeTemplates!))
-    );
-
-  ied.ownerDocument
-    .querySelectorAll(':root > DataTypeTemplates > EnumType')
-    .forEach(enumType =>
-      edits.push(<Edit>addEnumType(ied, enumType, dataTypeTemplates!))
-    );
-
-  return <Edit[]>edits.filter(item => item !== undefined);
-}
-
 function validateOrReplaceInput(tf: OscdTextfield): void {
   if (!(parseInt(tf.value, 10) >= 0 && parseInt(tf.value, 10) <= 99)) {
     // eslint-disable-next-line no-param-reassign
@@ -450,11 +119,16 @@ export default class ImportTemplateIedPlugin extends LitElement {
   @state()
   importDocs?: XMLDocument[] = [];
 
+  @state()
+  editCount = -1;
+
   @query('#importTemplateIED-plugin-input') pluginFileUI!: HTMLInputElement;
 
   @query('mwc-dialog') dialog!: Dialog;
 
-  @query('ul.icd-list') icdList!: HTMLUListElement;
+  @query('#icd-list') icdList!: HTMLUListElement;
+
+  @query('#comms-addresses') importCommsAddressesUI!: Checkbox;
 
   @property({ attribute: false })
   inputSelected = false;
@@ -463,6 +137,8 @@ export default class ImportTemplateIedPlugin extends LitElement {
   importIedCount: number | null = null;
 
   failFast = false;
+
+  errorString: string[] = [];
 
   async run(): Promise<void> {
     this.importDocs = [];
@@ -496,22 +172,15 @@ export default class ImportTemplateIedPlugin extends LitElement {
       const newIedName = uniqueTemplateIedName(this.doc, iedCopy);
       iedCopy.setAttribute('name', newIedName);
 
-      let edits: Edit[] = addDataTypeTemplates(iedCopy, this.doc);
-
+      // Update communication elements for new name
       Array.from(
         iedCopy.ownerDocument.querySelectorAll(
           ':root > Communication > SubNetwork > ConnectedAP[iedName="TEMPLATE"]'
         )
       ).forEach(connectedAp => connectedAp.setAttribute('iedName', newIedName));
-      edits = edits.concat(addCommunicationElements(iedCopy, this.doc));
 
-      edits.push({
-        parent: this.doc!.querySelector(':root')!,
-        node: iedCopy,
-        reference:
-          this.doc!.querySelector('IED') ??
-          this.doc!.querySelector('Communication')! ??
-          this.doc.querySelector(':root'),
+      const edits = insertIed(this.doc.documentElement, iedCopy, {
+        addCommunicationSection: this.importCommsAddressesUI.checked,
       });
 
       this.dispatchEvent(newEditEvent(edits));
@@ -529,7 +198,7 @@ export default class ImportTemplateIedPlugin extends LitElement {
       importQuantity,
       importDoc,
     ] of this.importDocs!.entries()) {
-      const templateIed = importDoc.querySelector(selector('IED', 'TEMPLATE'))!;
+      const templateIed = importDoc.querySelector('IED[name="TEMPLATE"]')!;
       const newIedCount = itemImportCountArray[importQuantity];
       if (newIedCount !== 0)
         await this.importTemplateIED(templateIed, newIedCount);
@@ -538,39 +207,22 @@ export default class ImportTemplateIedPlugin extends LitElement {
     this.dialog.close();
   }
 
-  // eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-unused-vars
   public isImportValid(templateDoc: Document, filename: string): boolean {
     if (!templateDoc) {
-      // TODO:
-      // this.dispatchEvent(
-      //   newLogEvent({
-      //     kind: 'error',
-      //     title: msg(str`Could not load file in ${filename}`),
-      //   })
-      // );
+      this.errorString.push(msg(`Could not load file in ${filename}`));
       return false;
     }
 
     if (templateDoc.querySelector('parsererror')) {
-      // TODO:
-      // this.dispatchEvent(
-      //   newLogEvent({
-      //     kind: 'error',
-      //     title: msg(str`Parser error in ${filename}`),
-      //   })
-      // );
+      this.errorString.push(msg(`Parser error in ${filename}`));
       return false;
     }
 
     const ied = templateDoc.querySelector(':root > IED[name="TEMPLATE"]');
     if (!ied) {
-      // TODO:
-      // this.dispatchEvent(
-      //   newLogEvent({
-      //     kind: 'error',
-      //     title: msg(str`No Template IED element in the file ${filename}`),
-      //   })
-      // );
+      this.errorString.push(
+        msg(`No Template IED element in the file ${filename}`)
+      );
       return false;
     }
     return true;
@@ -609,6 +261,7 @@ export default class ImportTemplateIedPlugin extends LitElement {
   protected renderInput(): TemplateResult {
     return html`<input multiple @change=${(event: Event) => {
       this.onLoadFiles(event);
+      // eslint-disable-next-line no-param-reassign
       (<HTMLInputElement>event.target).value = '';
     }} id="importTemplateIED-plugin-input" accept=".icd" type="file"></input>`;
   }
@@ -658,7 +311,10 @@ ${secondLine}"
   protected renderIedSelection(): TemplateResult {
     const iedImportCount = this.importIedCount ?? this.importDocs?.length ?? 0;
     return html`<mwc-dialog heading="${msg('Import Template IEDs')}">
-      <ul class="icd-list">
+      <mwc-formfield label="${msg('Include Communications Addresses')}">
+        <mwc-checkbox id="comms-addresses"></mwc-checkbox>
+      </mwc-formfield>
+      <ul id="icd-list">
         ${this.importDocs!.sort((a, b) => {
           const aSortstring = Array.from(
             Object.entries(getTemplateIedDescription(a))
